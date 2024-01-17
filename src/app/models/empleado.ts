ba9529319fc92cf413b0231ba2c52e5d5aa0200c
EstefaniaZhungo <113316595+EstefaniaZhungo@users.noreply.github.com>
2024-01-16 20:47:41 -0500

    Alquiler, categoria, empleado, auto, usuario_rol
    Alquiler, categoria, empleado, auto, usuario_rol

---
+++ src/app/models/empleado.ts
@@ -0,0 +1,6 @@
+export class Empleado {
+
+    id_empleado: number = 0;
+    id_persona: number = 0;
+    salario:number=0;
+}
---
+++ src/app/services/categoria.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Categoria } from '../models/categoria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  private urlEndPoint:string = 'http://localhost:8080/api/roles';
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Categoria[]>{
+    return this.http.get<Categoria[]>(this.urlEndPoint);
+  }
+
+  buscar(id_categoria: number):Observable<Categoria>{
+    return this.http.get<Categoria>('${this.urlEndPoint}/${id_categoria}');
+  }
+
+  crear(categoria: Categoria): Observable<Categoria>{
+    return this.http.post<Categoria>(this.urlEndPoint, categoria,{headers: this.httpHeaders})
+  }
+
+  editar(categoria: Categoria): Observable<Categoria> {
+    const id_categoria = '${this.urlEndPoint}/${categoria.id_categoria}';
+    return this.http.put<Categoria>(id_categoria, categoria, { headers: this.httpHeaders});
+  }
+
+  eliminar(id_categoria: number): Observable<Categoria>{
+    return this.http.delete<Categoria>('${this.urlEndPoint}/${id_categoria}')
+  }
+}
