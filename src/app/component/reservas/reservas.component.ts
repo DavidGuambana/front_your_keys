93506dc905ccd49547a7700cffe58e0221a2efdb
JosiasAldaz <josiasjoestar@hotmail.com>
2024-01-23 11:28:39 -0500

    componente

--- src/app/component/reservas/reservas.component.ts
+++ src/app/component/reservas/reservas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Alquiler } from 'src/app/models/alquiler';
 import { AutoService } from 'src/app/services/auto.service';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { ProteccionService } from 'src/app/services/proteccion.service';
 
 @Component({
   selector: 'app-reservas',
@@ -12,7 +13,8 @@ export class ReservasComponent implements OnInit{
 
   constructor(
     private ser_cliente:ClienteService,
-    private ser_auto:AutoService 
+    private ser_auto:AutoService ,
+    private ser_proteccion: ProteccionService
     ){
   }
   ngOnInit(): void {
